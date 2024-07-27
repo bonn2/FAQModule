f045f09906964a408cefa9c053362ecea72e17ec
Brett Bonifas <bonifas.fetzer@gmail.com>
2024-07-26 21:24:48 -0400

    Update /saves command

--- src/main/java/net/bonn2/faq/Command.java
+++ src/main/java/net/bonn2/faq/Command.java
@@ -16,19 +16,42 @@ public class Command extends ListenerAdapter {
         event.reply(new MessageCreateBuilder().addEmbeds(List.of(
                 new EmbedBuilder()
                         .setTitle("How to find your saves")
-                        .setDescription("Vectorio saves end in `.sav`")
+                        .setDescription("Vectorio saves end in `.sav` and should be located at:")
                         .addField(
                                 "Windows",
                                 """
-                                        They are located in:
-                                         `C:\\Users\\<user>\\AppData\\LocalLow\\Vectorio Remake\\Vectorio Remake\\Saves`
-                                        You can get there quickly by:
-                                        1. Pressing `Win+R`
-                                        2. Pasting in `%appdata%/../LocalLow/Vectorio Remake/Vectorio Remake/Saves`
-                                        3. Pressing `Enter`
-                                        4. Selecting the game mode you want to view""",
+                                        ```%appdata%/../LocalLow/Vectorio Remake/Vectorio Remake/World Saves/Live/```
+                                        """,
                                 true
                         )
+                        .addField(
+                                "Linux",
+                                """
+                                        ```~/.config/unity3d/Vectorio Remake/Vectorio Remake/World Saves/Live/```
+                                        """,
+                                true
+                        )
+                        .addField(
+                                "Mac",
+                                """
+                                        ```~/Library/Application Support/Vectorio Remake/Vectorio Remake/World Saves/Live/```
+                                        """,
+                                true
+                        )
+                        .addField(
+                                "Identifying saves",
+                                """
+                                        Saves are named world_`i`.sav, where `i` is a counter, usually your oldest save is `1`
+                                        **Back up** your save **before** doing this! You might accidentally delete something you shouldn't have.
+                                        1. Copy a save that you think could be the one you're looking for
+                                        2. Rename it from world_i.sav to world_i.gz. if you can't see the .sav extensions at the end of the name, turn on "file extensions" in your browser.
+                                        3. Right-click and extract/open it with a tool like 7Zip or WinZip, and you should see a new file with no extension, named the same as the save file is.
+                                        4. Open this file in a text editor
+                                        5. Use the Find shortcut, usually CTRL+F, and search for `Name:`
+                                        6. Your save file's name should be there.""",
+                                false
+                        )
+                        .setFooter("Thanks @com2486 for helping write this!")
                         .build()
         )).build()).queue();
     }
